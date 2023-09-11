de28dda131ca70b0a809b8ecc843e29ae225a0de
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2023-09-11 22:22:03 +0200

    Style merging

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import { css } from "#styled-system/css";
+import { SystemStyleObject } from "#styled-system/types";
+import { ReactNode } from "react";
+
+const Button = ({
+  css: cssProp = {},
+  children,
+}: {
+  css?: SystemStyleObject;
+  children: ReactNode;
+}) => {
+  const className = css({ display: "flex" }, cssProp);
+
+  return <button className={className}>{children}</button>;
+};
 
 export const App = () => (
-  <h1
-    className={css({
-      display: "block",
-      color: "primary",
-    })}
-  >
-    Hello world
-  </h1>
+  <>
+    <h1
+      className={css({
+        display: "block",
+        color: "primary",
+      })}
+    >
+      Hello world
+    </h1>
+    <Button css={css.raw({ color: "secondary" })}>Hi</Button>
+  </>
 );
